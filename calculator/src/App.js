2a4751ea96dd97325187856bef26e51887001042
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-02-02 20:52:19 +0200

    adding cases

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,10 +1,12 @@
+/* eslint-disable default-case */
 import './style.css';
 import {useReducer} from 'react';
 import DigitButton from './DigitButton';
+import OperationButton from './OperationButton';
 
 export const ACTION = {
 	ADD_DIGIT: 'add-digit',
-	CHOOSE_OPERANT: 'choose-operation',
+	CHOOSE_OPERATION: 'choose-operation',
 	CLEAR: 'clear',
 	DELETE_DIGIT: 'delete-digit',
 	EVALUATE: 'evaluate'
@@ -14,10 +16,30 @@ export const ACTION = {
 function reducer(state, {type, payload}){
 	switch(type){
 		case ACTION.ADD_DIGIT:
+			if(payload.digit ==="0" && state.currentOperant === "0") {
+				return state
+			}
+			if(payload.digit ==="." && state.currentOperant.includes('.')) {
+				return state
+			}
 			return {
 				...state,
-				currentOperant: `${state.currentOperant || ""}${payload.digit}`
+				currentOperant: `${state.currentOperant || ""}${payload.digit}`,
 			}
+		case ACTION.CHOOSE_OPERATION:
+			if(state.currentOperant == null && state.previousOpperant == null){
+				return {}
+			}
+			if(state.previousOpperant == null){
+				return {
+					...state,
+					operation: payload.opeartion,
+					previousOpperant: state.currentOperant,
+					currentOperant: null
+				}
+			}
+		case ACTION.CLEAR:
+			return {}
 	}
 };
 
@@ -36,21 +58,21 @@ function App() {
 			<div className="previous-operant">{previousOpperant} {opeartion}</div>
 			<div className="current-operant">{currentOperant}</div>
 		</div>
-		<button className="span-two">AC</button>
+		<button className="span-two" onClick={() => dispatch({type: ACTION.CLEAR})}>AC</button>
 		<button>DEL</button>
-		<DigitButton digit="÷" dispatch={dispatch}/>
+		<OperationButton operation="÷" dispatch={dispatch}/>
 		<DigitButton digit="1" dispatch={dispatch}/>
 		<DigitButton digit="2" dispatch={dispatch}/>
 		<DigitButton digit="3" dispatch={dispatch}/>
-		<DigitButton digit="*" dispatch={dispatch}/>
+		<OperationButton operation="*" dispatch={dispatch}/>
 		<DigitButton digit="4" dispatch={dispatch}/>
 		<DigitButton digit="5" dispatch={dispatch}/>
 		<DigitButton digit="6" dispatch={dispatch}/>
-		<DigitButton digit="+" dispatch={dispatch}/>
+		<OperationButton operation="+" dispatch={dispatch}/>
 		<DigitButton digit="7" dispatch={dispatch}/>
 		<DigitButton digit="8" dispatch={dispatch}/>
 		<DigitButton digit="9" dispatch={dispatch}/>
-		<DigitButton digit="-" dispatch={dispatch}/>
+		<OperationButton operation="-" dispatch={dispatch}/>
 		<DigitButton digit="." dispatch={dispatch}/>
 		<DigitButton digit="0" dispatch={dispatch}/>
 		<button className="span-two">=</button>
--- calculator/src/DigitButton.js
+++ calculator/src/DigitButton.js
@@ -3,5 +3,5 @@ import { ACTION } from "./App"
 
 export default function DigitButton({dispatch, digit}){
     
-    return <button onClick={() => dispatch({type: ACTION.ADD_DIGIT, payload: {digit}})}>{digit}</button>
+    return (<button onClick={() => dispatch({type: ACTION.ADD_DIGIT, payload: {digit}})}>{digit}</button>)
 }
